aeede5f850d1b33c0c61f9de60a0396d3e0c4161
PRASHANT KUMAR <68775732+prashantk137@users.noreply.github.com>
2025-06-24 13:34:29 +0530

    LeetCode - Second Highest Salary

---
+++ Second Highest Salary.sql
@@ -0,0 +1,78 @@
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+Schema :-
+
+Create table If Not Exists Employee (id int, salary int)
+Truncate table Employee
+insert into Employee (id, salary) values ('1', '100')
+insert into Employee (id, salary) values ('2', '200')
+insert into Employee (id, salary) values ('3', '300')
+
+Answer :-
+
+Method 1 -
+
+/* Write your T-SQL query statement below */
+with cte as(
+    select  distinct salary,
+    dense_rank() over(order by salary desc) as rnk
+    from Employee
+)
+select case when exists(select 1 from cte where rnk=2) then (select salary from cte where rnk =2) 
+else null 
+end as SecondHighestSalary;
+
+Mehtod 2 -
+
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (select max(salary) from Employee);
