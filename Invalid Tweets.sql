754f41acc4b465c71216d0cf92124dff0c128ead
PRASHANT KUMAR <68775732+prashantk137@users.noreply.github.com>
2025-07-04 13:28:31 +0530

    LeetCode - Invalid Tweets

---
+++ Invalid Tweets.sql
@@ -0,0 +1,29 @@
+Schema -
+
+Create table If Not Exists Tweets(tweet_id int, content varchar(50))
+Truncate table Tweets
+insert into Tweets (tweet_id, content) values ('1', 'Let us Code')
+insert into Tweets (tweet_id, content) values ('2', 'More than fifteen chars are here!');
+
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+
+Question -
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Answer -
+
+select tweet_id
+from Tweets
+where len(content)>15;
+
