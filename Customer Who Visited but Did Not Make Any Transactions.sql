dd04c758b3000a6a2384fdec63e6e2a066a79e3e
PRASHANT KUMAR <68775732+prashantk137@users.noreply.github.com>
2025-07-06 00:31:11 +0530

    LeetCode - 1581
    Customer Who Visited but Did Not Make Any Transactions

---
+++ Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,55 @@
+Schema -
+
+Create table If Not Exists Visits(visit_id int, customer_id int)
+Create table If Not Exists Transactions(transaction_id int, visit_id int, amount int)
+Truncate table Visits
+insert into Visits (visit_id, customer_id) values ('1', '23')
+insert into Visits (visit_id, customer_id) values ('2', '9')
+insert into Visits (visit_id, customer_id) values ('4', '30')
+insert into Visits (visit_id, customer_id) values ('5', '54')
+insert into Visits (visit_id, customer_id) values ('6', '96')
+insert into Visits (visit_id, customer_id) values ('7', '54')
+insert into Visits (visit_id, customer_id) values ('8', '54')
+Truncate table Transactions
+insert into Transactions (transaction_id, visit_id, amount) values ('2', '5', '310')
+insert into Transactions (transaction_id, visit_id, amount) values ('3', '5', '300')
+insert into Transactions (transaction_id, visit_id, amount) values ('9', '5', '200')
+insert into Transactions (transaction_id, visit_id, amount) values ('12', '1', '910')
+insert into Transactions (transaction_id, visit_id, amount) values ('13', '2', '970');
+
+Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+
+Table: Transactions
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+Question -
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Answer -
+
+select customer_id,count(*) as count_no_trans 
+from Visits v
+left join Transactions t
+on v.visit_id=t.visit_id
+where t.visit_id is null
+group by customer_id;
