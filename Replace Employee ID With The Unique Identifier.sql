47ecabab6c6588aea00966ff0cab21418a6842ee
PRASHANT KUMAR <68775732+prashantk137@users.noreply.github.com>
2025-07-05 20:01:14 +0530

    Replace Employee ID With The Unique Identifier
    LeetCode

---
+++ Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,48 @@
+Schema -
+
+Create table If Not Exists Employees (id int, name varchar(20))
+Create table If Not Exists EmployeeUNI (id int, unique_id int)
+Truncate table Employees
+insert into Employees (id, name) values ('1', 'Alice')
+insert into Employees (id, name) values ('7', 'Bob')
+insert into Employees (id, name) values ('11', 'Meir')
+insert into Employees (id, name) values ('90', 'Winston')
+insert into Employees (id, name) values ('3', 'Jonathan')
+Truncate table EmployeeUNI
+insert into EmployeeUNI (id, unique_id) values ('3', '1')
+insert into EmployeeUNI (id, unique_id) values ('11', '2')
+insert into EmployeeUNI (id, unique_id) values ('90', '3');
+
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+
+Answer -
+
+select EmployeeUNI.unique_id,Employees.name
+from Employees
+left join EmployeeUNI
+on Employees.id=EmployeeUNI.id;
